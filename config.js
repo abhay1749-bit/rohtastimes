6f754ca62460218aefbef7d22f4a3147a783eca4
abhay1749-bit <abhay1749@gmail.com>
2026-09-23 03:19:02 +0530

    config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = "https://mixeopjaqoqwmfiskbml.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "यहाँ अपनी copied key paste करो";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Rfi6jtxZ_in4qa13G015qw_m_W5WPYD"
